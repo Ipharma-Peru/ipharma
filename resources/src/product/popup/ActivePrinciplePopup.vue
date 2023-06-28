aceb18a815f2d4f907b28db7738cda8930ba03c9
Jose Luis Pintado Vasquez <joseluis.pintadovasquez@everis.nttdata.com>
2023-06-28 11:13:57 -0500

    feat: construcción de popUp

--- resources/src/product/popup/ActivePrinciplePopup.vue
+++ resources/src/product/popup/ActivePrinciplePopup.vue
@@ -1,15 +1,17 @@
 <template>
   <div
     class="modal fade"
-    id="laboratory"
+    id="activePrinciple"
     tabindex="-1"
-    aria-labelledby="laboratoryLabel"
+    aria-labelledby="activePrincipleLabel"
     aria-hidden="true"
   >
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="laboratoryLabel">Nuevo Laboratorio</h1>
+          <h1 class="modal-title fs-5" id="activePrincipleLabel">
+            Nuevo Principio Activo
+          </h1>
           <button
             type="button"
             class="btn-close"
@@ -20,10 +22,15 @@
         <div class="modal-body">
           <form>
             <div class="mb-3">
-              <label for="recipient-laboratory" class="col-form-label"
-                >Laboratorio:</label
+              <label for="recipient-activePrinciple" class="col-form-label"
+                >Principio Activo:</label
               >
-              <input type="text" class="form-control" id="recipient-laboratory" />
+              <input
+                type="text"
+                class="form-control"
+                id="recipient-activePrinciple"
+                v-model="nameactivePrinciple"
+              />
             </div>
           </form>
         </div>
@@ -32,13 +39,17 @@
             type="button"
             class="btn btn-secondary"
             data-bs-dismiss="modal"
-             @click="cerrarPopup"
-            
+            @click="cerrarPopup"
           >
             Close
           </button>
-          <button type="button" class="btn btn-primary" @click="agregarLaboratorio">Guardar</button>
-          
+          <button
+            type="button"
+            class="btn btn-primary"
+            @click="addactivePrinciple"
+          >
+            Guardar
+          </button>
         </div>
       </div>
     </div>
@@ -46,24 +57,35 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      nombreLaboratorio: "",
+      nameactivePrinciple: "",
     };
   },
   methods: {
     cerrarPopup() {
-      alert("h");
-      this.nombreLaboratorio = ""; // Reiniciar el campo de nombre del laboratorio al cerrar el pop-up
+      this.nameactivePrinciple = ""; // Reiniciar el campo de nombre del activePrinciple al cerrar el pop-up
       this.$emit("close"); // Emitir evento para indicar que se ha cerrado el pop-up
     },
-    agregarLaboratorio() {
-      // Realizar acciones para agregar el laboratorio con el nombre this.nombreLaboratorio
-      // Puedes emitir un evento para enviar los datos del laboratorio agregado a tu componente padre
-      this.$emit("laboratorioAgregado", this.nombreLaboratorio);
-      this.nombreLaboratorio = ""; // Reiniciar el campo de nombre del laboratorio después de agregarlo
-      this.$emit("close"); // Cerrar el pop-up después de agregar el laboratorio
+    addactivePrinciple() {
+      // Realizar la solicitud POST con axios
+      console.log(this.nameactivePrinciple)
+       debugger
+      axios
+        .post("/api/agregaractivePrinciple", { nombre_activePrinciple: this.nameactivePrinciple })
+        .then((response) => {
+          // Aquí puedes manejar la respuesta de la solicitud
+          // Puedes emitir un evento para enviar los datos del activePrinciple agregado a tu componente padre
+          this.$emit("activePrincipleAgregado", response.data);
+          this.nameactivePrinciple = ""; // Reiniciar el campo de nombre del activePrinciple después de agregarlo
+          this.$emit("close"); // Cerrar el pop-up después de agregar el activePrinciple
+        })
+        .catch((error) => {
+          console.error("Error al agregar el activePrinciple", error);
+        });
     },
   },
 };
