3f20867d0df4e94a228114b37388c2dbb2e9f1fd
Jose Luis Pintado Vasquez <joseluis.pintadovasquez@everis.nttdata.com>
2023-06-29 18:53:05 -0500

    feat: input sugerido para proveedor

--- resources/src/shopping/ShoppingCreate.vue
+++ resources/src/shopping/ShoppingCreate.vue
@@ -12,17 +12,31 @@
         <div class="card-body">
           <form @submit.prevent="addData">
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-7">
                 <label for="proveedor" class="form-label">Proveedor</label>
                 <div class="form-group">
                   <div class="input-group">
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="proveedor"
-                      v-model="formData.proveedores"
-                      placeholder="Proveedor"
-                    />
+                    <div class="full-row">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="proveedor"
+                        v-model="formData.proveedores"
+                        placeholder="Proveedor"
+                        @input="fetchSuggestions"
+                        @focus="toggleSuggestions(true)"
+                        @blur="toggleSuggestions(false)"
+                      />
+                      <ul v-if="showSuggestionList" class="suggestion-list">
+                        <li
+                          v-for="suggestion in suggestions"
+                          :key="suggestion"
+                          @mousedown="selectSuggestion(suggestion)"
+                        >
+                          {{ suggestion.razon_social }}
+                        </li>
+                      </ul>
+                    </div>
                     <button
                       type="button"
                       class="btn btn-primary"
@@ -34,14 +48,11 @@
                   </div>
                 </div>
               </div>
-            </div>
-            <ProveedorPopup
-               
-              />
-            <div class="row">
-              <div class="col-md-6">
+              <div class="col-md-3">
                 <div class="form-group">
-                  <label for="factura" class="form-label">Numero de Factura</label>
+                  <label for="factura" class="form-label"
+                    >Numero de Factura</label
+                  >
                   <input
                     type="text"
                     class="form-control"
@@ -51,7 +62,7 @@
                   />
                 </div>
               </div>
-              <div class="col-md-4">
+              <div class="col-md-2">
                 <div class="form-group">
                   <label for="fechaFactura" class="form-label"
                     >Fecha de Factura</label
@@ -66,6 +77,8 @@
                 </div>
               </div>
             </div>
+            <ProveedorPopup />
+            <div class="row"></div>
             <div class="row">
               <div class="container">
                 <table class="table">
@@ -107,7 +120,7 @@
                               v-for="option in item.options"
                               :key="option.id"
                               class="list-group-item"
-                              @click="selectItem(option, item)"
+                              @mousedown="selectItem(option, item)"
                             >
                               {{ option.nombre }}
                             </li>
@@ -176,14 +189,13 @@
   </div>
 </template>
 <script>
-import ProveedorPopup from "./popup/ProveedorPopup.vue"
-
+import ProveedorPopup from "./popup/ProveedorPopup.vue";
 
 import axios from "axios";
 
 export default {
   components: {
-    ProveedorPopup
+    ProveedorPopup,
   },
   data() {
     return {
@@ -205,10 +217,12 @@ export default {
       ],
       formData: {
         proveedores: "",
+        proveedorId: null,
         fechaFactura: "",
         factura: "",
       },
-      
+      suggestions: [],
+      showSuggestionList: false,
     };
   },
   computed: {
@@ -277,12 +291,7 @@ export default {
       this.fetchOptions(item);
     },
     handleBlur(item) {
-      setTimeout(() => {
-        if (!item.optionSelected) {
-          item.showSuggestions = false;
-        }
-        item.optionSelected = false;
-      }, 200);
+      item.showSuggestions = false;
     },
     selectItem(option, item) {
       item.articulo = option.nombre;
@@ -325,6 +334,29 @@ export default {
     calcularTotal(item) {
       return item.cantidad * item.precio;
     },
+    // -----Provedor Sugestion------
+    fetchSuggestions() {
+      const inputValue = this.formData.proveedores;
+
+      axios
+        .post("/api/proveedor/buscar", { search: inputValue })
+        .then((response) => {
+          console.log(response);
+          this.suggestions = response.data;
+          this.showSuggestionList = true;
+        })
+        .catch((error) => {
+          console.error("Error al obtener las sugerencias:", error);
+        });
+    },
+    toggleSuggestions(show) {
+      this.showSuggestionList = show && this.suggestions.length > 0;
+    },
+    selectSuggestion(suggestion) {
+      this.formData.proveedores = suggestion.razon_social;
+      this.formData.proveedorId = suggestion.id;
+      this.showSuggestionList = false;
+    },
   },
 };
 function removeAccents(text) {
@@ -344,5 +376,24 @@ function removeAccents(text) {
 .autocomplete-list li:hover {
   background-color: #435ebe34;
   cursor: pointer;
+}
+</style>
+<style>
+.suggestion-list {
+  position: absolute;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-top: none;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+.suggestion-list li {
+  padding: 5px 10px;
+  cursor: pointer;
+}
+.full-row {
+  width: 95%;
 }
 </style>
