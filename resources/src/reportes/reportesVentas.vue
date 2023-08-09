a8137390d229332b732311c9ef6b5ed1934f831a
Jose Luis Pintado Vasquez <joseluis.pintadovasquez@everis.nttdata.com>
2023-08-09 09:57:24 -0500

    feat: contrucción de reporte de ventas

---
+++ resources/src/reportes/reportesVentas.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <div class="row">
+      <div class="col-12 col-sm-12 col-lg-6" id="ventasDetail">
+        <div class="card-header">
+          <h4 class="card-title">Cliente</h4>
+        </div>
+        <div class="card-body">
+          <div class="row">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
---
+++ resources/views/admin/reportes/list.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+  <x-slot name="header">
+    </x-slot>
+
+    <div class="" id="listReportesVentas"></div>
+</x-app-layout>
